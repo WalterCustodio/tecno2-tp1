2360bbbd206d14082554b3915af73e77a9de4765
WalterCustodio <82871842+WalterCustodio@users.noreply.github.com>
2023-08-04 11:54:08 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ let gestorAmp;
 //----------Configuración
 let AMP_MIN = 0.004;
 let AMP_MAX = 0.05;
-let AMORTIGUACION = 0.94; // factor de amortiguación
+let AMORTIGUACION = 0.9; // factor de amortiguación
 // BOOLEANOS
 let haySonido = false;
 let antesHabiaSonido = false;
@@ -18,20 +18,20 @@ let IMPRIMIR = false;
 //
 //-----------------------------tiempo
 let marcaDeTiempo;
-let umbralDeTiempo = 2500;
-let unmbralSonido = 0.11;
+let umbralDeTiempo = 1500;
+let unmbralSonido = 0.31;
 let ultimoSonido;
 //
 //------------------sketch-----------------
 
 let figuras = [];
 let texturas = [];
-let cantidadDeFiguras = 35;
+let cantidadDeFiguras = 50;
 let cantidadDeTexturas = 10;
-let opbg = 30;
+let opBg = 30;
 
-let tamL = 2;
-let tamM = 1.7;
+let tamL = 1.7;
+let tamM = 1.5;
 let tamS = 1.5;
 
 
@@ -54,7 +54,7 @@ function setup(){
 
     createCanvas(width,height);
     pixelDensity(1)
-    frameRate(30)
+    frameRate(24)
     userStartAudio();
     //-------------GESTOR -----
     gestorAmp = new GestorSenial(AMP_MIN, AMP_MAX);
@@ -81,7 +81,7 @@ function draw(){
 
 gestorAmp.actualizar(mic.getLevel());  
 amp = gestorAmp.filtrada;
-//console.log(amp)
+//console.log(amp,"amp")
 
 haySonido = amp > AMP_MIN; 
 
@@ -89,92 +89,104 @@ let empezoElSonido = haySonido && !antesHabiaSonido;
 let terminoElsonido = antesHabiaSonido && !haySonido;
 
 if(empezoElSonido){
+  opBg= 30;
  marcaDeTiempo = millis();
 }
 if(terminoElsonido){
-
+  opBg = 150;
   let momentoActual = millis();
 
   if (momentoActual > marcaDeTiempo + umbralDeTiempo){
     ultimoSonido = "largo";
-    opbg = 100;
-   // console.log("largo")
+
+    console.log("largo")
   }
   if (momentoActual < marcaDeTiempo + umbralDeTiempo){
     ultimoSonido = "corto";
-
-    opbg = 30;
-   // console.log("corto")
+    console.log("corto")
   }
 }
 if (amp > unmbralSonido){
   reiniciarPrototipo();
 }
 //-------------
-  background(237,230,227,opbg);
+  background(237,235,227,opBg);
 
+  //------------- interactividad
   let fValorbajo = figuras.filter (figura => figura.opacity <100);
   let fValorMedio = figuras.filter (figura => figura.opacity >= 100 && figura.opacity<=150);
   let fValorAlto = figuras.filter (figura => figura.opacity > 150);
   //console.log("figuras = ",figuras.length,"altos =",fValorAlto.length, "medios =", fValorMedio.length, "bajos = ", fValorbajo.length)
 
-    for(let i=0;i<fValorbajo.length;i++){   //-------- FIGURAS DE VALOR BAJO
-   //  console.log("opacidad figura valor bajo",[i], "=",fValorbajo[i].opacity);
-     fValorbajo[i].setColor(195,190,180);
-     fValorbajo[i].dibujar(tamS);
-     if (ultimoSonido== "corto"){
-     fValorbajo[i].mover();
-     }
-     if(!haySonido){
-     fValorbajo[i].cambiarForma();
-     for(let j = 0; j<5; j++){
-     fValorbajo[i].cambiarPosicion();
-     }
-     }
-      }
+
      for(let i=0;i<fValorMedio.length;i++){   //-------- FIGURAS DE VALOR MEDIO
     // console.log("opacidad figura valor medio",[i], "=",fValorMedio[i].opacity);
-     fValorMedio[i].setColor(50,50, 45);
+     fValorMedio[i].setColor(55 ,50, 45);
      fValorMedio[i].dibujar(tamM);
      if(haySonido){
       fValorMedio[i].mover();
      }
       if (ultimoSonido== "largo"){
-        for(let j = 0; j<1; j++){
+        if(empezoElSonido){
      fValorMedio[i].cambiarForma();
-        }
+    }
       }
          }
 
-     for(let i=0;i<fValorAlto.length;i++){   //-------- FIGURAS DE VALOR ALTO
-     // console.log("opacidad figura valor alto",[i], "=",fValorAlto[i].opacity);
-     fValorAlto[i].setColor(25,27,30);
-      fValorAlto[i].dibujar(tamL);
-      fValorAlto[i].mover();
-     // fValorAlto[i].cambiarForma();
-      if(!haySonido){   //----------------------evento para mover y rotar
-      fValorAlto[i].bajarOpacidad();   
-      }
+
+     for(let i=0;i<fValorbajo.length;i++){   //-------- FIGURAS DE VALOR BAJO
+      //  console.log("opacidad figura valor bajo",[i], "=",fValorbajo[i].opacity);
+        fValorbajo[i].setColor(195,195,180);
+        fValorbajo[i].dibujar(tamS);
+        if(!haySonido){
+         fValorbajo[i].cambiarForma();
+        }
+        if (ultimoSonido== "corto"){
+         if(!haySonido){
+  
+          fValorbajo[i].cambiarPosicion();
+        
+        }
+     
+        }
+        if(ultimoSonido == "largo"){
+          fValorbajo[i].mover();
+        }
+  
          }
+         for(let i=0;i<fValorAlto.length;i++){   //-------- FIGURAS DE VALOR ALTO
+          // console.log("opacidad figura valor alto",[i], "=",fValorAlto[i].opacity);
+          fValorAlto[i].setColor(25,27,30);
+           fValorAlto[i].dibujar(tamL);
+           fValorAlto[i].mover();
+          // fValorAlto[i].cambiarForma();
+           if(haySonido){   //----------------------evento para mover y rotar
+           fValorAlto[i].bajarOpacidad();   
+           }
+              }
+
+              ////// -----------------
          antesHabiaSonido = haySonido;
   }
+  
 
 
 function crearTextura(graphics){
   graphics.push();
   graphics.pixelDensity(1);
     graphics.noStroke();
-    for (let n = 0; n < 22000; n++) {
+    for (let n = 0; n < 25000; n++) {
       let i = random(width);
       let j = random(height);
     graphics.fill(0,0,0,random(5,100));
-    graphics.circle(i,j,random(5,30));
+    graphics.circle(i,j,random(15,30));
     }
     graphics.pop();
 
   }
   function reiniciarPrototipo() {
     // Reiniciar figuras
+    opBg = 30;
     figuras = [];
     for (let i = 0; i < cantidadDeFiguras; i++) {
       let index = int(random(0, texturas.length));
